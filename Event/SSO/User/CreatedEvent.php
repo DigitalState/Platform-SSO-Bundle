23deb0786431e2eab068204df6487f0da50bf16a
mario-digitalstate <mario@digitalstate.ca>
2017-01-23 12:51:59 -0500

    Add resource owner to sso.user.created event.

--- Event/SSO/User/CreatedEvent.php
+++ Event/SSO/User/CreatedEvent.php
@@ -4,6 +4,7 @@ namespace Ds\Bundle\SSOBundle\Event\SSO\User;
 
 use Symfony\Component\EventDispatcher\Event;
 use Oro\Bundle\UserBundle\Entity\User;
+use HWI\Bundle\OAuthBundle\OAuth\ResourceOwnerInterface;
 
 /**
  * Class CreatedEvent
@@ -33,12 +34,31 @@ class CreatedEvent extends Event
     # endregion
 
     /**
+     * @var \HWI\Bundle\OAuthBundle\OAuth\ResourceOwnerInterface
+     */
+    protected $resourceOwner; # region accessors
+
+    /**
+     * Get resource owner
+     *
+     * @return \HWI\Bundle\OAuthBundle\OAuth\ResourceOwnerInterface
+     */
+    public function getResourceOwner()
+    {
+        return $this->resourceOwner;
+    }
+
+    # endregion
+
+    /**
      * Constructor
      *
      * @param \Oro\Bundle\UserBundle\Entity\User $user
+     * @param \HWI\Bundle\OAuthBundle\OAuth\ResourceOwnerInterface $resourceOwner
      */
-    public function __construct(User $user)
+    public function __construct(User $user, ResourceOwnerInterface $resourceOwner)
     {
         $this->user = $user;
+        $this->resourceOwner = $resourceOwner;
     }
 }
